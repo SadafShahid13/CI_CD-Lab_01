8a7bdeabfd798a730d72b861f6ca17defa3d299a
SadafShahid13 <borshon.sadafshahid@gmail.com>
2021-12-01 13:04:22 +0600

    7th

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/',(req,res)=>{
 
 
 
-app.listen(8070, ()=>{
-    console.log('server is listening on port 8070...')
+app.listen(3000, ()=>{
+    console.log('server is listening on port 3000...')
 })
